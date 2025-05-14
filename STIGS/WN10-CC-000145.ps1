5443a720a5ab57f6d545a0ac7020e9549200a0bf
JustinSoflin <js1212@aol.com>
2025-05-13 18:44:39 -0700

    Update WN10-CC-000145.ps1

--- STIGS/WN10-CC-000145.ps1
+++ STIGS/WN10-CC-000145.ps1
@@ -1,6 +1,29 @@
-WN10-CC-000145 - Users must be prompted for a password on resume from sleep (on battery).
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the users are prompted for a password on resume from sleep (on battery).
 
-# Requires administrative privileges
+.NOTES
+    Author          : Justin Soflin
+    LinkedIn        : linkedin.com/in/justin-soflin/
+    GitHub          : github.com/justinsoflin
+    Date Created    : 2025-05-13
+    Last Modified   : 2025-05-13
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000145
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\STIG-ID-WN10-CC-000145.ps1
+#>
 
 # Define the registry path
 $regPath = "HKLM:\SOFTWARE\Policies\Microsoft\Power\PowerSettings\0e796bdb-100d-47d6-a2d5-f7d2daa51f51"
