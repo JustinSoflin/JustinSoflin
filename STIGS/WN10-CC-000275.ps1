1fffa8cb7df96fa4adf69877b34c8bc34f54da98
JustinSoflin <js1212@aol.com>
2025-05-21 16:58:08 -0700

    Create WN10-CC-000275.ps1

---
+++ STIGS/WN10-CC-000275.ps1
@@ -0,0 +1,30 @@
+<#
+.SYNOPSIS
+
+This powershell script prevents local drives from sharing with Remote Desktop Session Hosts.
+
+.NOTES
+    Author          : Justin Soflin
+    LinkedIn        : linkedin.com/in/justin-soflin/
+    GitHub          : github.com/justinsoflin
+    Date Created    : 2025-05-21
+    Last Modified   : 2025-05-21
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000275 
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 	
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\STIG-ID-WN10-CC-000275.ps1
+#>
+
+New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" -Force
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" -Name "fDisableCdm" -Value 1 -Type DWord
