a15797f07f53985801b08d5f350db648f7b5cc45
JustinSoflin <js1212@aol.com>
2025-05-21 14:13:09 -0700

    Update WN10-CC-000030.ps1

--- STIGS/WN10-CC-000030.ps1
+++ STIGS/WN10-CC-000030.ps1
@@ -1,1 +1,30 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures the system prevents Internet Control Message Protocol (ICMP) redirects from overriding Open Shortest Path First (OSPF) generated routes.
 
+.NOTES
+    Author          : Justin Soflin
+    LinkedIn        : linkedin.com/in/justin-soflin/
+    GitHub          : github.com/justinsoflin
+    Date Created    : 2025-05-21
+    Last Modified   : 2025-05-21
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000030
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\STIG-ID-WN10-CC-000030.ps1
+#>
+
+# Set 'EnableICMPRedirect' to 0 to disable ICMP redirect acceptance
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" `
+  -Name "EnableICMPRedirect" -Value 0 -Type DWord
